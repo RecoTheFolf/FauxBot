ca3cf8361824de09f204d08a0a54fae6362c2239
Reconal <37031158+RecoTheFolf@users.noreply.github.com>
2018-05-25 17:08:32 -0400

    Changed the version to v0.2

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,7 @@ async run() {
     const Discord = require('discord.js');
 
     console.info(`${this.bot.user.username} is online and in ${this.bot.guilds.size} servers!`);
-    this.bot.user.setActivity("on Development Branch v0.1!");
+    this.bot.user.setActivity("on Development Branch v0.2!");
 
 }
 
