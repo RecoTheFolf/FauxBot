deeb51eee308b2bbe09538003d510757e2377535
Reconal <37031158+RecoTheFolf@users.noreply.github.com>
2018-05-29 21:13:17 -0400

    Added message.delete() before bulk

--- commands/staff/purge.js
+++ commands/staff/purge.js
@@ -18,6 +18,7 @@ class Purge extends Command {
 
     if (args[0] > 100) return message.channel.send('**Please supply a number less than 100**');
 
+    message.delete()
     message.channel.bulkDelete(args[0])
     .then( messages => message.channel.send(`**Successfully deleted \`${messages.size}/${args[0]}\` messages**`).then( msg => msg.delete({timeout: 2500})))
     .catch( error => message.channel.send(`**Error:** ${error.message}`));
