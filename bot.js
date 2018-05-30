28598da1dc047fe0c32139c4e8c10203b4ce1f11
Reconal <37031158+RecoTheFolf@users.noreply.github.com>
2018-05-30 01:56:25 -0400

    Woops, forgot to switch back

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ try {
 //load constants
 const { promisify } = require("util");
 const readdir = promisify(require("fs").readdir)
-const bottoken = require("./devtoken.json");
+const bottoken = require("./token.json");
 const Discord = require("discord.js");
 const r = require('rethinkdbdash')({db:`FauxBot`})();
 
