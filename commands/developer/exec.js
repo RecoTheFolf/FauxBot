65212e54b1f874ef8b7cbabdead6facd8e02a63a
Reconal <37031158+RecoTheFolf@users.noreply.github.com>
2018-06-01 13:26:03 -0400

    Added 1 more thing to exec

--- commands/developer/exec.js
+++ commands/developer/exec.js
@@ -16,6 +16,7 @@ async run(message, args, level) {
     let token = this.bot.token;
     let yttoken = this.bot.tokens.ytKey;
     let devtoken = this.bot.tokens.devToken;
+    let settings = this.bot.settings;
     let embed = new Discord.MessageEmbed()
   .setTitle("Evaluation")
   .setDescription("Sorry, the `exec` command can only be executed by the Bot Developers.")
@@ -25,6 +26,7 @@ async run(message, args, level) {
       stdout = stdout.replace(token, "No, bad!")
       stdout = stdout.replace(yttoken, "No, bad!")
       stdout = stdout.replace(devtoken, "No, bad!")
+      stdout = stdout.replace(settings, "No, bad!")
       const response = (error || stdout);
       message.channel.send(`Ran: ${args.join(" ")}\n${response}`, {code: "asciidoc", split: "\n"}).catch(console.error);
     });
