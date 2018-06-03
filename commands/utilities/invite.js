9ee32e30d4506a576bfa2d6ae0f2e2c85a997459
Reconal <37031158+RecoTheFolf@users.noreply.github.com>
2018-06-03 00:02:54 -0400

    Changed up invite just a little

--- commands/utilities/invite.js
+++ commands/utilities/invite.js
@@ -6,13 +6,15 @@ const perms = ['ADMINISTRATOR']
 class Invite extends Command {
   constructor(bot) {
       super(bot,{ 
-          name:'ping',
-          description: "Latency of the bot",
-          level:"Server Member"
+          name:'invite',
+          description: "Creates a bot invite so you can invite it to your server",
+          level:"Server Member",
+          usage: "invite"
       })
   }
 
 async run (message,args) {
+message.channel.send("Click on the link below to invite me to your server!")
 message.channel.send(await this.bot.generateInvite(perms))
 }
 }
