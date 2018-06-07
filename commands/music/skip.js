cb2eef6309f478c89ee0eee99a25f0b8669d5f43
Reconal <37031158+RecoTheFolf@users.noreply.github.com>
2018-06-06 20:29:03 -0400

    Added skip

---
+++ commands/music/skip.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const config = require("../../settings.js");
+const Command = require('../../base/Cmds.js');
+const youtube = require('simple-youtube-api')
+
+
+class Skip extends Command {
+  constructor(bot) {
+      super(bot,{ 
+          name:'skip',
+          description: "skips what is currently playing",
+          guildOnly: true,
+          level:"Server DJ",
+          usage: "skip",
+          perms:['CONNECT','SPEAK']
+      })
+  }
+async run (message, args) {
+if (!message.member.voiceChannel) return [0,'You must be in a voice channel to use this']
+message.guild.voiceConnection.dispatcher.end()
+message.channel.send(`Skipped!`)
+}
+}
+
+module.exports = Skip;
