6de758415e026e3544a30541f74c011a2a03c4c3
Reconal <37031158+RecoTheFolf@users.noreply.github.com>
2018-06-10 19:49:31 -0400

    Added a few new image commands

--- commands/fun/cat.js
+++ commands/fun/cat.js
@@ -17,21 +17,21 @@ class Cat extends Command {
   async run(message, args) { // eslint-disable-line no-unused-vars
     const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
 
-    request.get('http://thecatapi.com/api/images/get?format=src&type=png', {
-      
-    }, function(error, response, body) {
-        if(!error && response.statusCode == 200) {
+    try {
+      get('https://api.cheweybot.ga/cat').then(res => {
           const embed = new Discord.MessageEmbed()
           .setTitle("Random Cat :3")
           .setAuthor(member.user.tag, member.user.displayAvatarURL())
           .setColor(`RANDOM`)
-          .setImage(response.request.uri.href)
+          .setImage(res.body.data)
+          .setFooter("API provided by api.cheweybot.ga")
+          
           return message.channel.send({embed});
-        } else {
-          message.channel.send(`<:RedX:451263237434900491> An error occurred.  This is a problem with the API or post method.  This is not bot-related.`) + console.log(err);
-        }
-    })
+      });
+  } catch(err) {
+      return message.channel.send("An error occurred.  This is a problem with the API or post method.  This is not bot-related");
   }
+}
 }
 
 module.exports = Cat;
