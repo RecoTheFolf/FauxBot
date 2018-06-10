56e3e75cbdbb4803f1c9b3705f6a6f75b9e2f437
Reconal <37031158+RecoTheFolf@users.noreply.github.com>
2018-06-10 19:50:59 -0400

    Added a few new image commands

--- commands/fun/fantasy.js
+++ commands/fun/fantasy.js
@@ -3,11 +3,11 @@ const config = require("../../settings.js");
 const Command = require('../../base/Cmds.js')
 const { get } = require("snekfetch");
 
-class Dog extends Command {
+class Fantasy extends Command {
   constructor(bot) {
       super(bot,{ 
-          name:'dog',
-          description: "Get a cute dog image :3",
+          name:'nature',
+          description: "Get an random image of fantasy",
           guildOnly:true,
           usage: "",
           perms:['EMBED_LINKS','ATTACH_FILES']
@@ -21,9 +21,9 @@ async run(message, args) {
     const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
 
     try {
-        get('https://api.cheweybot.ga/dog').then(res => {
+        get('https://api.cheweybot.ga/fantasy-art').then(res => {
             const embed = new Discord.MessageEmbed()
-            .setTitle("Random Dog :3")
+            .setTitle("Fantasy!")
             .setAuthor(member.user.tag, member.user.displayAvatarURL())
             .setColor(`RANDOM`)
             .setImage(res.body.data)
@@ -36,4 +36,4 @@ async run(message, args) {
     }
 }
 }
-module.exports = Dog;
+module.exports = Fantasy;
