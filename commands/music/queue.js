7f6a721a5ea505ca74d5aee78d8780598075c96d
Reconal <37031158+RecoTheFolf@users.noreply.github.com>
2018-06-16 18:19:08 -0400

    Changed up queue

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,24 +1,43 @@
 const Discord = require("discord.js");
-const Command = require('../../base/Cmds.js');
+const Command = require("../../base/Cmds.js");
+const perpage = 10
 
 class Queue extends Command {
-  constructor(bot) {
-      super(bot,{ 
-          name:'queue',
-          description: "Music Queue",
-          usage: "",
-          guildOnly:true
-      })
-  }
+    constructor(bot) {
+        super(bot, {
+            name:'queue',
+            description: "Music Queue",
+            usage: "[page-num]",
+            guildOnly:true,
+            level:"Server Member"
+        });
+    }
 
-async run(message, args, level) {
-const queue = message.guild.streamData.get('queue')
-if (queue.length === 0) return [0,"Nothing is currently in the queue"]
+    async run(message, args, level) {
+        const bot = this.bot
+        try {
+            if (!message.guild.me.voiceChannel) return message.channel.send(message.author.toString() + " :x: There is nothing playing");
+            const queue = message.guild.streamData.get('queue');
+            if (!queue || queue.length === 0) return message.channel.send(message.author.toString() + " :x: There is nothing playing")
+            let page = parseInt(args[0]) > 0 && parseInt(args[0]) <= Math.ceil(queue.length / perpage) ? parseInt(args[0]) : 1
+            let output = ""
+            let count = 0
+            queue.forEach(song => {
+                if (count < perpage * page && count > perpage * page - (perpage + 1)) {
+                    output = output + `\n${count + 1} = **${song.title} (Requested by ${song.requester.tag ? song.requester.tag : `${song.requester.username}#${song.requester.discriminator}`}) **`
+                }
+                count ++
+            })
+            return message.channel.send(output)
+            this.bot.sendMessage(message, {
+                title: "Music Queue",
+                description: `${output}`
+            }, true)
+        } catch (err) {
+            message.channel.send('' + err)
+        }
+    }
 
-message.channel.send(`Queue:\n${queue.map(q => `\`${q.title}\` | Requested by \`${q.requester.tag}\``).join('\n')}`)
-
-
-}
 }
 
 module.exports = Queue;
