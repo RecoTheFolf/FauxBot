86afb576ff4fa694f6f0e40e1c6973c7e849c7e2
Reconal <37031158+RecoTheFolf@users.noreply.github.com>
2018-07-02 08:59:22 -0400

    Changed so Urban is only used in NSFW channels

--- commands/fun/urban.js
+++ commands/fun/urban.js
@@ -16,28 +16,32 @@ class Urban extends Command {
   }
 
 async run(message, args, tools) {
-    if (!args[0]) return message.channel.send("***Please specifiy some text!***");
-
-    let res = await urban(args.join(' ')).catch(e => {
-        return message.channel.send("***That word was not found!***");
-    });
-    const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
-    const embed = new Discord.MessageEmbed()
-    .setTitle("FauxBot Urban")
-    .setAuthor(member.user.tag, member.user.displayAvatarURL())
-    .setColor(`RANDOM`)
-    .setURL(res.urbanURL)
-    .setDescription(`**Definition:**\n*${res.definition}*\n\n**Example:**\n*${res.example}*`)
-    .addField('Author', res.author, true)
-    .addField('Rating', `**\`Upvotes: ${res.thumbsUp} | Downvotes: ${res.thumbsDown}\`**`)
-
-    if (res.tags.length > 0 && res.tags.join(', ').length < 1024) {
-        embed.addField('Tags', res.tags.join(', '), true)
+    if (message.channel.nsfw === false) {
+    return message.channel.send("<:RedX:451263237434900491> This channel isn't marked as NSFW.");
     }
+    else {
+        if (!args[0]) return message.channel.send("<:RedX:451263237434900491> Please specifiy some text!");
 
-    message.channel.send(embed);
-
+        let res = await urban(args.join(' ')).catch(e => {
+            return message.channel.send("<:RedX:451263237434900491> That word was not found!");
+        });
+        const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
+        const embed = new Discord.MessageEmbed()
+        .setTitle("FauxBot Urban")
+        .setAuthor(member.user.tag, member.user.displayAvatarURL())
+        .setColor(`RANDOM`)
+        .setURL(res.urbanURL)
+        .setDescription(`**Definition:**\n*${res.definition}*\n\n**Example:**\n*${res.example}*`)
+        .addField('Author', res.author, true)
+        .addField('Rating', `**\`Upvotes: ${res.thumbsUp} | Downvotes: ${res.thumbsDown}\`**`)
     
+        if (res.tags.length > 0 && res.tags.join(', ').length < 1024) {
+            embed.addField('Tags', res.tags.join(', '), true)
+        }
+    
+        message.channel.send(embed);
+    }
+ 
 }
 }
 
